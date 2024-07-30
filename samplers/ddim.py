42e65dacdcfb051a6feecc20511fe49f777e7462
bensha6757 <ben6757@gmail.com>
2024-07-30 21:19:15 +0300

    DDIM and stable diffusion 2.1

--- samplers/ddim.py
+++ samplers/ddim.py
@@ -74,6 +74,9 @@ class DDIMSampler(nn.Module):
         prev_t = max(0, t - 1)
         return self.sample_one_step(latents, t, prev_t, eta=kwargs.get('eta', 0.0))
 
+    def scale_model_input(self, sample: torch.Tensor, *args, **kwargs) -> torch.Tensor:
+        return sample
+
     @torch.no_grad()
     def forward(self, x_t, eta=0.0, only_return_x_0: bool = True, interval: int = 1):
         steps = self.num_inference_steps
